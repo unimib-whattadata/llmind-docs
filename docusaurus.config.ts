f05c6506bc05c8b211581c73f305790563b3319a
Fabio <fabio.dadda@unimib.it>
2025-02-19 16:34:00 +0100

    add new index page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,65 +1,65 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
-import tailwindPlugin from './plugins/tailwind-plugin.cjs';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import tailwindPlugin from "./plugins/tailwind-plugin.cjs";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://unimib-whattadata.github.io/',
+  url: "https://unimib-whattadata.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/llmind-docs/',
+  baseUrl: "/llmind-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Unimib Whattadata', // Usually your GitHub org/user name.
-  projectName: 'LLMind Docs', // Usually your repo name.
+  organizationName: "Unimib Whattadata", // Usually your GitHub org/user name.
+  projectName: "LLMind Docs", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -67,34 +67,34 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'LLMind Docs',
+      title: "LLMind Docs",
       logo: {
-        alt: 'LLMind Logo',
-        src: 'img/logo.svg',
+        alt: "LLMind Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Documentation',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Documentation",
         },
         {
-          href: 'https://unimib-datai.github.io/sti-website/',
-          label: 'Our works',
-          position: 'right',
+          href: "https://unimib-datai.github.io/sti-website/",
+          label: "Our works",
+          position: "right",
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} Whattadata srl. Built with Docusaurus.`,
     },
     prism: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 import React from "react";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
@@ -45,118 +45,97 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title="Decoding the Mind: A RAG-LLM on ICD-11 for
-        Decision Support in Psychology"
+      title="Decoding the Mind: A RAG-LLM on ICD-11 for Decision Support in Psychology"
       description="Research Paper Summary"
     >
-      <head>
-      <link rel="stylesheet" href="/custom.css" />
-      </head>
-      <div className="hero hero--primary">
-        <div className="container">
-          <div className="banner-container">
-            {/* Left side (logo and title) */}
-            <div className="left-side">
+      <main>
+        <img
+          src="/llmind-docs/img/bblurry_1.svg"
+          className="absolute opacity-15 -top-[45rem]"
+        />
+        <section className="relative z-10">
+          <div className="place-content-center mx-4 lg:gap-12 xl:gap-12 lg:pt-10 lg:grid-cols-12 pb-12">
+            <h1 className="md:text-center pt-12 mb-4 leading-none text-gray-900 sm:text-6xl md:text-7xl lg:text-8xl dark:text-white font-bold tracking-widest">
+              LLMind
+            </h1>
+            <div className="justify-center flex flex-col md:flex-row max-w-6xl gap-20 pb-2 mx-auto">
               <img
-                src="https://picsum.photos/200/300"
                 alt="Logo"
-                className="logo"
+                src="/llmind-docs/img/logo.svg"
+                className="lg:mt-0 lg:col-span-4 lg:flex w-[20rem] object-fit-cover h-auto max-w-full"
               />
-              <div>
-                <h1 className="hero__title">
-                  Decoding the Mind: A RAG-LLM on ICD-11
-                </h1>
-                <p className="hero__subtitle">
-                  Decision Support in Psychology
+              <div className="place-content-center max-w-lg pl-2">
+                <p className=" text-dgBackground dark:text-white sm:text-xl lg:mb-8 md:text-2xl lg:text-3xl">
+                  {" "}
+                  Decoding the Mind: A RAG-LLM on ICD-11 for Decision Support in
+                  Psychology{" "}
                 </p>
+                <div className="flex flex-row">
+                  <a
+                    href="/llmind-docs/docs/introduction"
+                    className="text-center inline-flex items-center border-none dark:bg-primaryGreen dark:text-lightBackground  hover:text-dgBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-secondaryGreen font-semibold"
+                  >
+                    Learn more <ArrowRight className="ml-2 h-5 w-5" />{" "}
+                  </a>
+                  <a
+                    href="https://forms.gle/oQ24aYpVZ5B8enXj8"
+                    className="text-center inline-flex items-center border-none dark:bg-primaryGreen dark:text-lightBackground  hover:text-dgBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-secondaryGreen font-semibold"
+                  >
+                    Feedback and Features request{" "}
+                    <ExternalLink className="ml-2 h-5 w-5" />{" "}
+                  </a>
+                  <a
+                    href="https://llmind.datai.disco.unimib.it/"
+                    className="text-center inline-flex items-center border-2 border-solid dark:border-primaryGreen border-secondaryGreen hover:text-dgBackground dark:text-lightBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 font-semibold"
+                  >
+                    Try it out <ExternalLink className="ml-2 h-5 w-5" />{" "}
+                  </a>
+                </div>
               </div>
             </div>
-
-            {/* Right side (buttons) */}
-            <div className="right-side">
-              <button className="button button--secondary button--lg margin-right--md">
-                Learn More
-              </button>
-              <button className="button button--secondary button--lg margin-right--md">
-                Feedback
-              </button>
-              <button className="button button--secondary button--lg">
-                Try It Out
-              </button>
+          </div>
+        </section>
+        <div className="overflow-hidden dark:bg-darkBackground bg-lightBackground py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="flex flex-col justify-center lg:pr-8 lg:pt-4">
+                <div className="lg:max-w-lg">
+                  <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                    A RAG-LLM on ICD-11 for Decision Support in Psychology
+                  </h2>
+                  <p className="mt-4 text-lg leading-6 text-gray-600 dark:text-gray-300">
+                    This paper investigates the potential of Large Language
+                    Models (LLMs) in assisting psychologists and psychiatrists
+                    with diagnostic decision-making using the ICD-11
+                    classification manual.
+                  </p>
+                  <div className="mt-6 max-w-xl space-y-4 text-base leading-5 text-gray-600 lg:max-w-none">
+                    {features.map((feature) => (
+                      <div key={feature.name} className="relative pl-8">
+                        <div className="inline font-bold text-gray-900 dark:text-gray-300">
+                          <feature.icon
+                            aria-hidden="true"
+                            className="absolute left-1 top-1 h-5 w-5 text-primaryGreen dark:text-secondaryGreen"
+                          />
+                          {feature.name}
+                        </div>
+                        <span className="inline pl-3 text-gray-600 dark:text-gray-200">
+                          {feature.description}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <img
+                alt="Product screenshot"
+                src="/llmind-docs/img/llmindUI.png"
+                className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="container margin-vert--xl">
-        <h1>
-          Decoding the Mind: A RAG-LLM on ICD-11 for Decision Support in
-          Psychology
-        </h1>
-
-
-        <h2>Abstract</h2>
-        <p>
-          This paper investigates the potential of Large Language Models (LLMs)
-          in assisting psychologists and psychiatrists with diagnostic
-          decision-making using the ICD-11 classification manual.
-        </p>
-
-        <h2>Introduction</h2>
-        <p>
-          Artificial Intelligence (AI) has made significant advancements, but its
-          use in mental health, particularly in psychiatry and psychology,
-          requires further research. This paper explores the potential of LLMs
-          for psychological and psychiatric diagnoses using the ICD-11 manual as
-          a comprehensive classification of mental health disorders.
-        </p>
-
-        <Tabs defaultValue="llmind-chat">
-          <TabItem value="llmind-chat" label="LLMind Chat">
-            <p>
-              The authors propose LLMind Chat, an AI-powered tool designed to
-              support mental health professionals in their diagnostic processes.
-              LLMind Chat utilizes a Retrieval Augmented Generation (RAG) model
-              based on Gemma 2, adapted to the context of the ICD-11. This RAG
-              model combines the strengths of Gemma 2 with a knowledge base
-              derived from the ICD-11, allowing it to access and process
-              relevant information from the classification manual in real-time.
-            </p>
-          </TabItem>
-          <TabItem value="evaluation-results" label="Evaluation and Results">
-            <p>
-              LLMind's diagnostic accuracy was evaluated against the DSM-5-TR
-              Clinical Cases manual using automated metrics and expert
-              validation from mental health professionals. The results
-              demonstrate its potential as a reliable and valid support tool for
-              clinicians.
-            </p>
-          </TabItem>
-          <TabItem
-            value="implications-future-work"
-            label="Implications and Future Work"
-          >
-            <p>
-              The integration of LLMs into clinical practice offers promising
-              opportunities to enhance mental health care. LLMs can serve as
-              decision-support tools, aiding in diagnostic accuracy and
-              treatment planning. Future work will focus on refining LLMind's
-              performance and expanding its capabilities.
-            </p>
-          </TabItem>
-        </Tabs>
-
-        <h2>Code Example</h2>
-        <CodeBlock language="python">
-          {`# Example code snippet
-          def get_diagnosis(patient_description):
-            # Preprocess patient description
-            #...
-            # Query LLMind model
-            diagnosis = llmind_model.query(patient_description)
-            #...
-            return diagnosis`}
-        </CodeBlock>
-      </div>
+      </main>
     </Layout>
   );
 }
